ebc225d7662137161edcc09b0a3234893455abcc
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-03-03 10:35:56 +0900

    B: build test

---
+++ docs/precache-manifest.d740209676e6c20e18aa7752bd931ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6a1b244b65a680b9a86",
+    "url": "/css/app.e62e89db.css"
+  },
+  {
+    "revision": "d6a1b244b65a680b9a86",
+    "url": "/js/app.8473ecbb.js"
+  },
+  {
+    "revision": "96a78c23ff05eba4a582",
+    "url": "/js/chunk-vendors.37d0f239.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b38ea73ab09e74542cac604da8a61ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gccc-site/precache-manifest.dd1a6be2b3b0cff738906a8e3bbad195.js"
+  "/precache-manifest.d740209676e6c20e18aa7752bd931ff4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gccc-site"});
