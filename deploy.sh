b7ffbd91cc9959f4a073a19a5835863391ef3e20
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-03-03 10:33:35 +0900

    Deploy setting

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 git checkout source
 echo "gccc.dev" > ./docs/CNAME
+git add -A ./docs/CNAME
+git commit -m "deploy CNAME"
 git push -f --delete origin master
 git subtree push --prefix docs/ origin master
