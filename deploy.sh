8f6ed9f9935546410019dd80b059cfe6e30bf940
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-03-08 08:59:57 +0900

    D: deploy shell

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git checkout source
-echo "gccc.dev" > ./docs/CNAME
-git add -A ./docs/CNAME
-git commit -m "deploy CNAME"
-git push -f --delete origin master
-git subtree push --prefix docs/ origin master
