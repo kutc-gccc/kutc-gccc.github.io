4718a168c4f29a65ac46457d4d74b0a1577470ef
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-03-09 18:40:33 +0900

    F: add empty @types to avoid typescript error

--- gccc-site/src/components/contents/home/TwitterBar.vue
+++ gccc-site/src/components/contents/home/TwitterBar.vue
@@ -19,12 +19,12 @@ import { Tweet, Moment, Timeline } from "vue-tweet-embed";
   },
 })
 export default class TwitterBar extends Vue {
+  @Prop() private twitterId!: string;
+  @Prop() private profile!: string;
+  @Prop() private tweetLimit!: number;
   public id: string = this.twitterId;
   public sourceType: string = this.profile;
   public limit: number = this.tweetLimit;
-  @Prop() private twitterId!: string;
-  @Prop() private profile!: string;
-  @Prop() private tweetLimit: number;
 }
 </script>
 
