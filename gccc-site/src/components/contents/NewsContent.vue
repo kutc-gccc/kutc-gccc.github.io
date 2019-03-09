43fa35aefdd1a71e34beabfb7865fb36827a7d03
Shimaoka Shuya <45023631+HiragiGKUTH@users.noreply.github.com>
2019-03-09 13:28:07 +0900

    Add overall structure (#7)
    * I: apply lint
    
    * A: vue-route and structure are done
    
    * F: debug

---
+++ gccc-site/src/components/contents/NewsContent.vue
@@ -0,0 +1,29 @@
+<template>
+	<section id="news-content">
+    <p>New page is not implemented yet :/</p>
+	</section>
+</template>
+
+<script lang="ts">
+// import decorator below
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+// import components below
+
+@Component({
+  components: {
+    // use components here
+  },
+})
+
+export default class NewsContent extends Vue {
+  public created() {
+    const t: string = "";
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+// scoped is affect only this components
+</style>
