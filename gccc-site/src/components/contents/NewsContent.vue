224d00987a8807a1ee8971107c3256823ec86925
Shimaoka Shuya <45023631+HiragiGKUTH@users.noreply.github.com>
2019-04-05 18:09:37 +0900

    Add news list (#14)
    * update npm
    
    * A: article components and implement news system

--- gccc-site/src/components/contents/NewsContent.vue
+++ gccc-site/src/components/contents/NewsContent.vue
@@ -1,24 +1,52 @@
 <template>
 	<section id="news-content">
-    <p>New page is not implemented yet :/</p>
+    <Article :articleHtml="newsData" :articleTitle="newsTitle"></Article>
 	</section>
 </template>
 
 <script lang="ts">
 // import decorator below
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Watch, Vue } from "vue-property-decorator";
+import Article from "./news/Article.vue";
+import axios from "axios";
+const API_ENDPOINT: string = "https://gccc-site-api.herokuapp.com";
 
-// import components below
+interface NewsData {
+  id: number;
+  title: string;
+  html: string;
+}
 
 @Component({
   components: {
-    // use components here
+    Article,
   },
 })
-
 export default class NewsContent extends Vue {
-  public created() {
-    const t: string = "";
+  private newsData: NewsData | null = null;
+  private newsTitle: string | null = null;
+
+  public async created() {
+    const res = await axios({
+      method: "GET",
+      url: API_ENDPOINT + "/api/" + this.$route.params.id,
+      headers: {"Access-Conrol-Allow-Origin": "true"},
+      responseType: "json",
+    });
+    console.log(res);
+    this.newsData = res.data.html;
+    this.newsTitle = res.data.title;
+  }
+
+  @Watch("$route")
+  public async fetchNewsData() {
+    const res = await axios({
+      method: "GET",
+      url: API_ENDPOINT + "/api/" + this.$route.params.id,
+      headers: {"Access-Conrol-Allow-Origin": "true"},
+      responseType: "json",
+    });
+    this.newsData = res.data.html;
   }
 }
 </script>
